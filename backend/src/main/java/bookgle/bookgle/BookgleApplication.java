67360e26189b3972c1e8579ffd70451b865a65ea
ldw <leehan02@hanyang.ac.kr>
2025-01-27 08:03:39 +0900

    [BE] create spring project

---
+++ backend/src/main/java/bookgle/bookgle/BookgleApplication.java
@@ -0,0 +1,13 @@
+package bookgle.bookgle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookgleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookgleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/bookgle/bookgle/BookgleApplicationTests.java
@@ -0,0 +1,13 @@
+package bookgle.bookgle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookgleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
