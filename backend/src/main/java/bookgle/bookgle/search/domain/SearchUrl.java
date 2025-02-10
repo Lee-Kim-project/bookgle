c61c470b6dbe5a01289fad4ebae3f0a3644427d3
ldw <leehan02@hanyang.ac.kr>
2025-02-10 17:55:42 +0900

    [BE] (domain) 도서관별 대출 가능여부 조회 API에 대한 url 상수로 추가

--- backend/src/main/java/bookgle/bookgle/search/domain/SearchUrl.java
+++ backend/src/main/java/bookgle/bookgle/search/domain/SearchUrl.java
@@ -11,7 +11,9 @@ public enum SearchUrl {
     // 도서 소장 도서관 조회
     ALL_LIBRARY_HAS_THE_BOOK("/libSrchByBook"),
     // 도서 정보 조회
-    BOOKS_INFO("/srchBooks");
+    BOOKS_INFO("/srchBooks"),
+    // 도서관별 대출 가능여부 조회
+    IS_BOOK_AVAILABLE("/bookExist");
 
 
     private final String url;
