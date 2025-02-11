f678c00705d7b999776806201b6d49ef18755114
ldw <leehan02@hanyang.ac.kr>
2025-02-12 04:57:46 +0900

    [BE] (controller) ServiceException이 발생할 경우 예외처리 로직 구현

--- backend/src/main/java/bookgle/bookgle/search/controller/SearchController.java
+++ backend/src/main/java/bookgle/bookgle/search/controller/SearchController.java
@@ -1,5 +1,6 @@
 package bookgle.bookgle.search.controller;
 
+import bookgle.bookgle.exception.ServiceException;
 import bookgle.bookgle.search.config.NaverMapsProperties;
 import bookgle.bookgle.search.domain.Book;
 import bookgle.bookgle.search.domain.Library;
@@ -26,14 +27,25 @@ public class SearchController {
 
     // 유저가 검색한 책 제목과 동일한 책들의 목록을 찾아서 보여줍니다.
     @GetMapping("/search/books")
-//    @ResponseBody // html을 반환하지 않고, 데이터 자체를 반환함
     public String searchBooks(Model model, String title, String[] region) throws IOException {
         // 유저가 입력한 책 제목으로 isbn을 검색한다.
-        List<Book> books = searchService.searchBooks(title);
+        List<Book> books;
+        try {
+            books = searchService.searchBooks(title);
+        } catch (ServiceException e) {
+            return "error.html";
+        }
+
         // 유저가 입력한 책 제목과 동일한 제목의 책 목록을 보여준다.
         model.addAttribute("books", books);
         model.addAttribute("booksCount", books.size());
-        model.addAttribute("regions", region);
+
+        // 유저가 지역을 선택하지 않았을 경우 서울이 default 값
+        if (region == null)
+            model.addAttribute("regions", "서울");
+        else
+            model.addAttribute("regions", region);
+
         return "bookList.html";
     }
 
@@ -47,13 +59,16 @@ public class SearchController {
         model.addAttribute("libraries", libraries);
         model.addAttribute("clientId", naverMapsProperties.getClientId());
         model.addAttribute("isbn", isbn);
+
         return "map.html";
     }
 
+    // 유저가 선택한 도서관에서 유저가 빌리고자 하는 책의 대출가능여부를 보여줍니다.
     @GetMapping("/search/isAvailable")
     @ResponseBody
     public String checkAvailable(String libCode, String isbn) {
         boolean result = searchService.isBookAvailable(libCode, isbn);
+
         if (result)
             return "대출 가능";
         return "대출 불가";
--- backend/src/main/java/bookgle/bookgle/search/domain/Book.java
+++ backend/src/main/java/bookgle/bookgle/search/domain/Book.java
@@ -3,13 +3,7 @@ package bookgle.bookgle.search.domain;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true) // json파일의 속성들 중에서 Book 클래스에 정의된 속성들만 가져온다.
@@ -17,13 +11,14 @@ public class Book {
     private final String title;
     private final String authors;
     private final String publisher;
-    private final int publicationYear;
+    private final String publicationYear;
     private final String isbn;
 
     @JsonCreator // json에서 Book class로 역직렬화할때 이 생성자를 사용하도록 함
     public Book(@JsonProperty("bookname") String bookname, @JsonProperty("authors") String authors,
-                @JsonProperty("publisher") String publisher, @JsonProperty("publication_year") int publicationYear,
+                @JsonProperty("publisher") String publisher, @JsonProperty("publication_year") String publicationYear,
                 @JsonProperty("isbn13") String isbn) {
+
         this.title = bookname;
         this.authors = authors;
         this.publisher = publisher;
