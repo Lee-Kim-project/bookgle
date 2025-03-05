0657c7f6f6ffcc4f0dc6a035db17af2b18cd5327
ldw <leehan02@hanyang.ac.kr>
2025-03-05 13:29:14 +0900

    fix: library class의 속성들에서 final 키워드 제거

--- backend/src/main/java/bookgle/bookgle/search/domain/Library.java
+++ backend/src/main/java/bookgle/bookgle/search/domain/Library.java
@@ -8,27 +8,31 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @ToString
+@NoArgsConstructor
 @Getter
+@Setter
 @JsonIgnoreProperties(ignoreUnknown = true) // json에 있는 속성들 중에서 Library 클래스에 정의된 속성들만 가져온다.
 public class Library {
     @Id
-    private final String code;
+    private String code;
 
     @Column(nullable = false)
-    private final String name;
+    private String name;
 
-    private final String address;
-    private final String tel;
-    private final String latitude;
-    private final String longitude;
-    private final String homepage;
+    private String address;
+    private String tel;
+    private String latitude;
+    private String longitude;
+    private String homepage;
 
     @Column(columnDefinition = "TEXT")
-    private final String operatingTime;
+    private String operatingTime;
 
     @JsonCreator
     public Library(@JsonProperty("libCode") String code, @JsonProperty("libName") String name, @JsonProperty("address") String address,
