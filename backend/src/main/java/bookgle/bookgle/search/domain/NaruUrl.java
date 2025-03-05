880b1d2a817fac9ca7fb9213c07e73241078a560
ldw <leehan02@hanyang.ac.kr>
2025-03-05 13:30:56 +0900

    fix: naruUrl class에 인터넷 프로토콜과 호스트 관련 상수 추가

--- backend/src/main/java/bookgle/bookgle/search/domain/NaruUrl.java
+++ backend/src/main/java/bookgle/bookgle/search/domain/NaruUrl.java
@@ -2,24 +2,31 @@ package bookgle.bookgle.search.domain;
 
 import lombok.Getter;
 
-@Getter
-public enum SearchUrl {
-    // base url
-    BASE_URL("http://data4library.kr/api"),
+//@Getter
+public enum NaruUrl {
+    // scheme
+    SCHEME("http"),
+    // host
+    HOST("data4library.kr"),
+//    BASE_URL("http://data4library.kr/api"),
+//    BASE_URL("/api"),
     // 도서관별 소장 도서 조회
-    BOOKS_BY_LIBRARY("/itemSrch"),
+    BOOKS_BY_LIBRARY("/api/itemSrch"),
     // 도서 소장 도서관 조회
-    ALL_LIBRARY_HAS_THE_BOOK("/libSrchByBook"),
+    ALL_LIBRARY_HAS_THE_BOOK("/api/libSrchByBook"),
     // 도서 정보 조회
-    BOOKS_INFO("/srchBooks"),
+    BOOKS_INFO("/api/srchBooks"),
     // 도서관별 대출 가능여부 조회
-    IS_BOOK_AVAILABLE("/bookExist"),
+    IS_BOOK_AVAILABLE("/api/bookExist"),
     // 정보공개 도서관 조회
-    ALL_LIBRARIES("/libSrch");
+    ALL_LIBRARIES("/api/libSrch");
 
 
     private final String url;
-    SearchUrl(String url) {
+    NaruUrl(String url) {
         this.url = url;
+    }
+    public String get() {
+        return url;
     }
 }
