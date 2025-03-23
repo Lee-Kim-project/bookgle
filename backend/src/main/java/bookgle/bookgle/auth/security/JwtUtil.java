66488702f4fb8d377bf0c2c860c6284b5ea0b211
ldw <leehan02@hanyang.ac.kr>
2025-03-24 01:48:53 +0900

    feat: JWT 관련 유틸 클래스 생성

---
+++ backend/src/main/java/bookgle/bookgle/auth/security/JwtUtil.java
@@ -0,0 +1,44 @@
+package bookgle.bookgle.auth.security;
+
+import bookgle.bookgle.auth.config.TokenProperties;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final SecretKey secretKey;
+
+    public JwtUtil(TokenProperties tokenProperties) {
+        secretKey = new SecretKeySpec(tokenProperties.getSecret().getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+//        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    // jwt에서 username 추출
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    // jwt에서 role 추출
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJwt(String username, String role, Long expiredMs) {
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
