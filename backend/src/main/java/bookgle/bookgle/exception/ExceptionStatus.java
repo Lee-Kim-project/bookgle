bf8e969b593ed6fb93d1624d85ec1c62c8f2080a
ldw <leehan02@hanyang.ac.kr>
2025-03-24 21:28:30 +0900

    fix: 오타 수정

--- backend/src/main/java/bookgle/bookgle/exception/ExceptionStatus.java
+++ backend/src/main/java/bookgle/bookgle/exception/ExceptionStatus.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionStatus {
     NOT_FOUND_BOOK(HttpStatus.NOT_FOUND, "검색한 책이 존재하지 않습니다."),
-    가(HttpStatus.CONFLICT, "이미 존재하는 유저입니다.");
+    ALREADY_REGISTERED_USER(HttpStatus.CONFLICT, "이미 존재하는 유저입니다.");
 
     final int statusCode;
     final String message;
