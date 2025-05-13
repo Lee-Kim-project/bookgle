1fc927a505cb2f5960ca9ec44c04c64db8267d87
ldw <leehan02@hanyang.ac.kr>
2025-05-13 15:54:17 +0900

    feat: 책 검색 api에서 프론트로 보낼때 사용할 dto 정의

---
+++ backend/src/main/java/bookgle/bookgle/dto/SearchBooksDto.java
@@ -0,0 +1,17 @@
+package bookgle.bookgle.dto;
+
+import bookgle.bookgle.search.domain.Book;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class SearchBooksDto {
+    private List<Book> books;
+    private int booksSize;
+}
