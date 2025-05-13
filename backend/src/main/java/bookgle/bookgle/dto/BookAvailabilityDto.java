431ad11edd8337a29a06052f1f548e8655b4add7
ldw <leehan02@hanyang.ac.kr>
2025-05-13 15:54:55 +0900

    feat: 대출 가능 여부 확인 api에서 사용할 dto 정의

---
+++ backend/src/main/java/bookgle/bookgle/dto/BookAvailabilityDto.java
@@ -0,0 +1,12 @@
+package bookgle.bookgle.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class BookAvailabilityDto {
+    private String message;
+}
